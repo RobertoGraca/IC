c59b0b664ab1183ddd01fbf0117f6de989fc97ee
Miguel Cabral <miguel.f.cabral@ua.pt>
2021-11-19 23:46:26 +0000

    changes in ex6.cpp

--- partec/ex6.cpp
+++ partec/ex6.cpp
@@ -9,19 +9,14 @@ int main(int argc, char *argv[])
 {
     AudioFile<double> audio;
     bool loaded = audio.load(argv[1]);
-    AudioFile<double> audio2 = audio;
     map<double, int> m0;
     map<double, int> m1;
-    map<double, int> m2;
     ofstream ofs0("ex6channel0.txt");
     ofstream ofs1("ex6channel1.txt");
-    //ofstream ofs2("ex6channelmono.txt");
     int totalSamples0 = 0;
     int totalSamples1 = 0;
-    int totalSamples2 = 0;
     double entropy0 = 0;
     double entropy1 = 0;
-    double entropy2 = 0;
     double prob = 0;
 
     /* If you hit this assert, then the file path above probably doesn't refer to a valid audio file */
@@ -53,13 +48,13 @@ int main(int argc, char *argv[])
         totalSamples1++;
     }
 
-    /* audio2.setNumChannels(1);                   //pass audio file to mono
+    /* audio2.setNumChannels(1);                                                     
     for (int nSamples = 0; nSamples < audio2.getNumSamplesPerChannel(); nSamples++)
     {
         if (m2.count(audio2.samples[1][nSamples]) == 0)
         {
             m2[audio2.samples[0][nSamples]] = 1;
-            cout<<"if1"<<endl;
+            cout<<"if1"<<endl;                                                         //it doesn't work
         }
         else
         {
@@ -86,12 +81,6 @@ int main(int argc, char *argv[])
     
     prob = 0;
 
-    for (auto x : m2)
-    {
-        prob = (double)(x.second) / totalSamples2;
-        entropy2 += prob*log(prob);
-    }
-
     for (auto x : m0)
     {
         ofs0 << x.first << " => " << x.second << endl;
@@ -111,10 +100,7 @@ int main(int argc, char *argv[])
     cout<<"Entropy of channel 0: "<<entropy0<<endl;
     entropy1 = -entropy1;
     cout<<"Entropy of channel 1: "<<entropy1<<endl;
-    entropy2 = -entropy2;
-    //cout<<"Entropy of channel mono: "<<entropy2<<endl;
     ofs0.close(); 
     ofs1.close(); 
-    //ofs2.close(); 
-    return 0; //to see the histogram run ex6.py and open ex6channel0.png
+    return 0;           //to see the histogram run ex6.py and open ex6channel0.png and ex6channel1.png
 }
