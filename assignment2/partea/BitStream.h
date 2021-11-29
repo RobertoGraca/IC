105d14e978535f4b77c5e3f4cf1eb41ce2ed7b03
Roberto <robertograca@ua.pt>
2021-11-29 15:53:46 +0000

    Read_bit Done(?)

--- assignment2/partea/BitStream.h
+++ assignment2/partea/BitStream.h
@@ -4,46 +4,105 @@ using namespace std;
 class BitStream
 {
 public:
-    bool buffer[8];
+    bool *buffer;
     FILE *file;
+    const char *perm;
+    int bit_count;
+    char current_char;
 
     BitStream(const char *path, const char *perm)
     {
-        this->file = fopen(path, perm);
+        if (*perm != 'r' && *perm != 'w')
+        {
+            throw "Illegal Permission Exception!";
+        }
+        this->perm = perm;
+        this->file = fopen(path, this->perm);
+        this->bit_count = 0;
     }
 
     bool read_bit()
     {
-        char c = fgetc(this->file);
-        return (c >> 0) & 1;
+        if (this->bit_count == 0)
+        {
+            this->current_char = fgetc(this->file);
+        }
+
+        if (this->current_char == EOF)
+        {
+            return false;
+        }
+
+        *buffer = (this->current_char >> this->bit_count) & 0x01;
+        buffer++;
+        this->bit_count = (this->bit_count + 1) % 8;
+
+        return true;
     }
 
-    bool* read_n_bits(int n)
+    bool *read_n_bits(int n)
     {
-        // Algo de errado não está certo aqui...
-        char c = fgetc(this->file);
-        for(int i = 0; i<n; i++){
-            buffer[i] = (c >> i) & 1;
-            cout << buffer[i];
+        bool read_n[n];
+        int i;
+        for (i = 0; i < n; i++)
+        {
+            read_n[i] = this->read_bit();
         }
-        return buffer;
+        return read_n;
     }
 
     bool write_bit(bool bit)
     {
-        // TODO
-        return 0;
+        *buffer = bit;
+        buffer++;
+
+        if (this->can_write())
+        {
+            fputc(this->bool_array_to_char(), this->file);
+            return true;
+        }
+        return false;
     }
 
-    bool write_n_bits(int b)
+    bool write_n_bits(bool *c)
     {
-        // TODO
-        return 0;
+        bool read;
+        while (*c != '\0')
+        {
+            read = this->write_bit(*c);
+            if (!read)
+                return false;
+        }
+        return true;
     }
 
     bool close()
     {
         fclose(this->file);
         return 0;
+    }
+
+    bool buffer_is_empty()
+    {
+        return !this->buffer;
+    }
+
+    bool can_write()
+    {
+        return ((sizeof(this->buffer) / this->buffer[0]) >= 8);
+    }
+
+    char bool_array_to_char()
+    {
+        int n = 0;
+        int i;
+        for (i = 0; i < 8; i++)
+        {
+            if (this->buffer[i])
+            {
+                n |= (1 << (8 - i));
+            }
+        }
+        return (char)n;
     }
 };
