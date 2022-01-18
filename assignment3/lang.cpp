42e674ab4b92b7088497534b49d54d2a9de79903
João <jtsimoes@ua.pt>
2022-01-18 02:28:26 +0000

    Improved exercise 1 of part B

--- assignment3/lang.cpp
+++ assignment3/lang.cpp
@@ -2,23 +2,40 @@
 
 int main(int argc, char **argv)
 {
-    if (argc == 3)
+    if (argc != 5)
     {
-        // no order context model and no probability estimator given
-    }
-    else if (argc == 4)
-    {
-        // no probability estimator given
-    }
-    else if (argc == 5)
-    {
-        // all arguments given
-    }
-    else
-    {
-        cout << "USAGE: ./lang <file_1> <file_2> [order_model] [probability_estimator]" << endl;
+        cout << "USAGE: ./lang <reference_text> <target_text> <order_model> <smoothing_parameter>" << endl;
         exit(1);
     }
 
-    return 0;
+    int estimated_n_bits = 0;
+    char c;
+
+    // create a model of the reference text
+    FCM ref_text(atoi(argv[3]), atoi(argv[4]));
+
+    // read reference text file
+    ref_text.read_file(argv[1]);
+
+    // open target text file
+    ifstream ifs(argv[2]);
+    if (!ifs.good())
+    {
+        cout << "File \"" << argv[2] << "\" could not be located." << endl;
+        exit(1);
+    }
+
+    /* while  PERCORRER TODOS OS CARACTERES E CHAMAR A FUNÇÃO GET_CTX E SOMAR */
+    while (ifs.get(c))
+    {
+        /* estimated_n_bits += - log2(probability char); */
+        estimated_n_bits += 1;
+    }
+
+    // close target text file
+    ifs.close();
+
+    cout << "" << estimated_n_bits << "" << endl;
+
+    return estimated_n_bits;
 }
